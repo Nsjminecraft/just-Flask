ce3409f9514af68aca381d1fd2431595da346993
Nsjminecraft <nerfgunredusebook@gmail.com>
2025-07-09 11:18:00 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/page2")
+def page2():
+    return render_template("page2.html")
+
+@app.route("/page3")
+def page3():
+    return render_template("page3.html")
+
+@app.route("/logo")
+def logo():
+    return render_template("logo.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
+
